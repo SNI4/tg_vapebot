fa8f842a972a078afd41b7b41244681786d6ddb0
SNI4 <artemhonest@gmail.com>
2022-11-07 20:49:15 +0300

    ver-3

--- handlers/users/offer.py
+++ handlers/users/offer.py
@@ -1,7 +1,10 @@
+import re
+
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
-from loader import dp
+from data import config
+from loader import dp, bot
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from utils.send_offer_to_admins import send_offer_to_admin_chat
@@ -13,8 +16,21 @@ class FSMOffer(StatesGroup):
 
 @dp.message_handler(commands=["offer"])
 async def offer(message: types.Message):
-    await FSMOffer.offer.set()
-    await message.answer("Отправьте мне ваше объявление.")
+
+    user_channel_status = await bot.get_chat_member(chat_id=config.CHANNEL_ID, user_id=message.chat.id)
+    user_channel_status = re.findall(r"\w*", str(user_channel_status))
+    try:
+        if user_channel_status[70] != 'left':
+            await FSMOffer.offer.set()
+            await message.answer("Отправьте мне ваше объявление.")
+        else:
+            await message.answer('Вы не подписаны на канал!\nt.me/brxlkpsd')
+    except:
+        if user_channel_status[60] != 'left':
+            await FSMOffer.offer.set()
+            await message.answer("Отправьте мне ваше объявление.")
+        else:
+            await message.answer('Вы не подписаны на канал!\nt.me/brxlkpsd')
 
 
 @dp.message_handler(state=FSMOffer.offer)
