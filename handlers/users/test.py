ee28150cf026c89d9f0b9d72621d1175a34a8172
SNI4 <artemhonest@gmail.com>
2022-11-27 21:08:58 +0300

    ver-4(input albums)

--- handlers/users/test.py
+++ handlers/users/test.py
@@ -1,30 +1,35 @@
 import asyncio
-
 from loader import dp, bot
 from aiogram import types
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import json
-
 class FSMTest(StatesGroup):
+
     media = State()
 
-@dp.message_handler(commands=['load'], state=None)
-async def test(message: types.Message):
+@dp.message_handler(commands=['load'])
+async def test(message: types.Message, state: FSMContext):
     await FSMTest.media.set()
+    await state.update_data(media=[])
     await message.reply('wait for photo(s)...')
 
-@dp.message_handler(content_types=['any'], state=FSMTest)
-async def load(message: types.Message, state=FSMTest):
-    med = json.loads(message.as_json())
-    media_list = []
-    # https://ru.stackoverflow.com/questions/1198860/Сохранение-всех-фото-из-альбома-без-message-handlercontent-types-photo/1202083#1202083
-    if 'media_group_id' in med:
-        for pic in med['photo']:
-            pic_id = pic['file_id']
-            media_list.append(pic_id) if pic_id not in media_list else ...
-            await message.reply(' ; '.join(media_list))
-            await state.finish()
-    else:
-        await message.reply(message.photo[-1].file_id)
+@dp.message_handler(content_types=['any'], state=FSMTest.media)
+async def load(message: types.Message, state: FSMContext):
+
+    if message.text != 'loaded':
+        async with state.proxy() as data:
+            data['media'].append(message.photo[-1].file_id)
+
+
+    elif message.text == 'loaded':
+        await message.reply('finished')
+        async with state.proxy() as data:
+
+            group = types.MediaGroup()
+            async for photo_id in data['media']:
+                group.attach_photo(photo_id)
+            await bot.send_media_group(message.chat.id, group)
+
         await state.finish()
+
